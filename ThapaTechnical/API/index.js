3f7824d05238564c796ae785ef36c41532695345
abhii718 <abhinaysingh47639@gamil.com>
2024-03-04 19:51:53 +0530

    weather app using API

--- ThapaTechnical/API/index.js
+++ ThapaTechnical/API/index.js
@@ -1,12 +1,82 @@
+let cityName = document.querySelector(".weather_city");
+let dateTime = document.querySelector(".weather_date_time");
+let w_forecast = document.querySelector(".weather_forecast");
+let w_icon = document.querySelector(".weather_icon");
+let w_temperature = document.querySelector(".weather_temperature");
+let w_minTem = document.querySelector(".weather_min");
+let w_maxTem = document.querySelector(".weather_max");
+
+let w_feelsLike = document.querySelector(".weather_feelsLike");
+let w_humidity = document.querySelector(".weather_humidity");
+let w_wind = document.querySelector(".weather_wind");
+let w_pressure = document.querySelector(".weather_pressure");
+
+let citySearch = document.querySelector(".weather_search");
+
 // to get the actual country name
-
-// to get the date and time
-
-// search functionality
-
-//define the getWeather function here
-const getWeatherData = async () => {
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=addyourownapikey`;
+const getCountryName = (code) => {
+  return new Intl.DisplayNames([code], { type: "region" }).of(code);
 };
 
-document.body.addEventListener("load", getWeatherData());
+// to get the date and time
+const getDateTime = (dt) => {
+  const curDate = new Date(dt * 1000); // Convert seconds to milliseconds
+  console.log(curDate);
+  //const date = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  console.log(formatter);
+  return formatter.format(curDate);
+};
+
+let city = "pune";
+
+// search functionality
+citySearch.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let cityName = document.querySelector(".city_name");
+  console.log(cityName.value);
+  city = cityName.value;
+  getWeatherData();
+  cityName.value = "";
+});
+
+// Function to fetch weather data
+const getWeatherData = async () => {
+  const apiKey = "ae2d027d63b2b5c8e8106497763f1214"; // Replace with your OpenWeatherMap API key
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
+
+  try {
+    const res = await fetch(weatherUrl);
+    const data = await res.json();
+    console.log(data);
+
+    const { main, name, weather, wind, sys, dt } = data;
+
+    // Update HTML elements with weather data
+    cityName.innerHTML = `${name}, ${getCountryName(sys.country)}`;
+    dateTime.innerHTML = getDateTime(dt);
+
+    w_forecast.innerHTML = weather[0].main;
+    w_icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather[0].icon}@4x.png" />`;
+
+    w_temperature.innerHTML = `${(main.temp - 273.15).toFixed(1)}&#176;C`;
+    w_minTem.innerHTML = `Min: ${(main.temp_min - 273.15).toFixed(1)}&#176;C`;
+    w_maxTem.innerHTML = `Max: ${(main.temp_max - 273.15).toFixed(1)}&#176;C`;
+    w_feelsLike.innerHTML = `${(main.feels_like - 273.15).toFixed(1)}&#176;C`;
+    w_humidity.innerHTML = `${main.humidity}%`;
+    w_wind.innerHTML = `${wind.speed} m/s`;
+    w_pressure.innerHTML = `${main.pressure} hPa`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+getWeatherData();
