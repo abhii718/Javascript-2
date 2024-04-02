5c99575b02bd09151302beb883b816bd855ba09f
abhii718 <abhinaysingh47639@gamil.com>
2024-04-02 11:01:55 +0530

    date

--- ForinterViwesPurPose/js.js
+++ ForinterViwesPurPose/js.js
@@ -174,38 +174,57 @@
 // console.log(sum(1, 2, 3)); // Output: 6
 
 // Creating an object using object literal
-const person = {
-    name: "John",
-    age: 30,
-    greet: function() {
-      console.log("Hello, " + this.name + "!");
-    }
-  };
-  
-  // Accessing object properties
-  console.log(person.name); // Output: John
-  console.log(person["age"]); // Output: 30
-  
-  // Adding a new property
-  person.city = "New York";
-  
-  // Modifying a property
-  person.age = 35;
-  
-  // Deleting a property
-  delete person.age;
-  
-  // Calling a method
-  person.greet(); // Output: Hello, John!
-  
-  console.log(person.age)
+// const person = {
+//     name: "John",
+//     age: 30,
+//     greet: function() {
+//       console.log("Hello, " + this.name + "!");
+//     }
+//   };
 
-  // Iterating over object properties
-const keys = Object.keys(person);
-console.log(keys); // Output: ["name", "age", "greet", "city"]
+//   // Accessing object properties
+//   console.log(person.name); // Output: John
+//   console.log(person["age"]); // Output: 30
 
-// Merging objects
-const additionalInfo = { job: "Developer" };
-const mergedObject = Object.assign({}, person, additionalInfo);
-console.log(mergedObject);
+//   // Adding a new property
+//   person.city = "New York";
 
+//   // Modifying a property
+//   person.age = 35;
+
+//   // Deleting a property
+//   delete person.age;
+
+//   // Calling a method
+//   person.greet(); // Output: Hello, John!
+
+//   console.log(person.age)
+
+//   // Iterating over object properties
+// const keys = Object.keys(person);
+// console.log(keys); // Output: ["name", "age", "greet", "city"]
+
+// // Merging objects
+// const additionalInfo = { job: "Developer" };
+// const mergedObject = Object.assign({}, person, additionalInfo);
+// console.log(mergedObject);
+
+// Create a new Date object with the desired date
+
+const hello = () => {
+  var date = new Date(2024, 10, 3);
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+  var formattedDate =
+    (month < 10 ? "0" : "") +
+    month +
+    "/" +
+    (day < 10 ? "0" : "") +
+    day +
+    "/" +
+    year;
+  console.log(formattedDate);
+};
+let hi = hello();
+console.log("hi world");
